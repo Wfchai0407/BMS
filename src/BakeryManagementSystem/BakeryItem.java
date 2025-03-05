62a8813e35a4e23ed5483fc3e1f3cae4d1cc29b7
Ray <Ray@RAY-LAPTOP>
2025-03-05 17:29:48 +0800

    add set & get methods

--- src/BakeryManagementSystem/BakeryItem.java
+++ src/BakeryManagementSystem/BakeryItem.java
@@ -4,14 +4,38 @@ public class BakeryItem
 {
     private String itemName;
     private double price;
-    private int quantityAvailable;
+    private int quantity;
 
-    public BakeryItem(String itemName, double price, int quantityAvailable) 
+    public BakeryItem(String itemName, double price, int quantity) 
     {
         this.itemName = itemName;
         this.price = price;
-        this.quantityAvailable = quantityAvailable;
+        this.quantity = quantity;
+    }
+    
+    public void updateQuantity(int quantity)
+    {
+    	this.quantity = quantity;
+    }
+    
+    public void updatePrice(double price)
+    {
+    	this.price = price;
+    }
+    
+    public String getItemName()
+    {
+    	return itemName;
+    }
+    
+    public double getPrice()
+    {
+    	return price;
+    }
+    
+    public int getQuantity()
+    {
+    	return quantity;
     }
 
-    // Getters, Setters, display method, etc.
 }
