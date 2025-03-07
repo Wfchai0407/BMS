3a34c25c89961b0f75e62b2a38f44e695852dad9
Ray <Ray@RAY-LAPTOP>
2025-03-07 16:10:44 +0800

    s

---
+++ bms/Main.java
@@ -0,0 +1,8 @@
+package bms;
+
+public class Main {
+	public static void main(String[] args) {
+        BakeryManagementSystem system = new BakeryManagementSystem(); // Create the system
+        system.startSystem(); // Start running the menu
+    }
+}
