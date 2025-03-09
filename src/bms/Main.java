b4afb47f4d5a74e7b43a26d0faa02753817c4d99
veson <veson@LAPTOP-OQUKSN74>
2025-03-09 21:23:55 +0800

    push(2)

--- src/bms/Main.java
+++ src/bms/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
         BakeryManagementSystem system = new BakeryManagementSystem();
         system.startSystem(); // Start running the menu
-    }
+        System.out.println("Testing to push");
+	}
 }
